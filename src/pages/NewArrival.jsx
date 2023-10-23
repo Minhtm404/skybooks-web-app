aa666ef75530462dd06f2675ccc2e7278c5e453a
Minh Tam <caominhtam.10a4@gmail.com>
2023-10-23 17:52:08 +0700

    change new arrival

--- src/pages/NewArrival.jsx
+++ src/pages/NewArrival.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Breadcrumb } from 'flowbite-react';
+import React, { useContext, useEffect } from 'react';
+import { Breadcrumb, Card } from 'flowbite-react';
 import { HiHome } from 'react-icons/hi';
+
+import { Context as ProductContext } from '../contexts/ProductContext';
+
 import { ProductBar } from '../components/index';
 
 const NewArrival = () => {
+  const { products, getAllProducts } = useContext(ProductContext);
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div>
       <Breadcrumb
@@ -23,7 +32,23 @@ const NewArrival = () => {
 
         <ProductBar />
 
-        <div className="grid grid-cols-4 gap-4"></div>
+        <div className="grid grid-cols-4 gap-4">
+          {products.map(p => (
+            <Card
+              imgAlt="Meaningful alt text for an image that is not purely decorative"
+              imgSrc="https://product.hstatic.net/200000090679/product/a11eaq8thkl_757a0ddca7b74dc7b30dfd32680930be_grande.jpg"
+            >
+              <h5 className="font-bold tracking-tight text-gray-900">
+                <p className="uppercase">
+                  {p.name.length > 25 ? p.name.slice(0, 25).concat('...') : p.name}
+                </p>
+              </h5>
+              <p className="font-normal text-gray-700">
+                <p>{p.price.toLocaleString().concat('₫')}</p>
+              </p>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
