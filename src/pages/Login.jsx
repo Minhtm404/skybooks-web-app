b756dddcd34c4086f76ac202d609986bdb5392ee
Minh Tam <caominhtam.10a4@gmail.com>
2023-11-16 18:02:35 +0700

    change login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Checkbox, Label, Spinner, TextInput, Toast } from 'flowbite-react';
+import { HiExclamation } from 'react-icons/hi';
+
+import { Context as AuthContext } from '../contexts/AuthContext';
 
 const Login = () => {
+  const { login, isLoading, setIsLoading, error } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState(undefined);
   const [password, setPassword] = useState(undefined);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if (!email || !password) return;
+
+    setIsLoading(true);
+    await login({ email, password });
+
+    navigate('/');
+  };
 
   return (
     <form
       className="grid content-center justify-center gap-4 my-16"
       style={{ 'grid-template-columns': '30rem' }}
+      onSubmit={handleSubmit}
     >
+      {error ? (
+        <Toast className="absolute top-4 left-4">
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
+            <HiExclamation className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">{error}</div>
+          <Toast.Toggle />
+        </Toast>
+      ) : (
+        <></>
+      )}
+
       <h3 className="text-4xl font-bold text-center text-gray-900 dark:text-white">
         Login
       </h3>
@@ -53,7 +84,17 @@ const Login = () => {
         </Link>
       </div>
       <div className="w-full">
-        <Button>Log in to your account</Button>
+        <Button type="submit">
+          <div className="flex flex-row gap-2">
+            {!isLoading ? (
+              ''
+            ) : (
+              <Spinner aria-label="Alternate spinner button example" size="sm" />
+            )}
+
+            <span>Log in to your account</span>
+          </div>
+        </Button>
       </div>
       <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
         Not registered?&nbsp;
